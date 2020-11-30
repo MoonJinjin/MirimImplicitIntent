5975478a4874ac59b47431ed388304db0fb1ac1f
MoonJinjin <s2019s11@e-mirim.hs.kr>
2020-11-30 19:46:16 +0900

    Complete OnClickListener(Button)

--- app/src/main/java/kr/hs/emirim/s2019s11/mirimimplicitintent/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s11/mirimimplicitintent/MainActivity.java
@@ -2,7 +2,11 @@ package kr.hs.emirim.s2019s11.mirimimplicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
@@ -31,7 +35,36 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
+            Intent intent = null;
+            Uri uri = null;
+            switch (v.getId()) {
+                case R.id.btn_call:
+                    uri = Uri.parse("tel:01033337777");
+                    intent = new Intent(Intent.ACTION_DIAL, uri);
+                    break;
+                case R.id.btn_homepage:
+                    uri = Uri.parse("http://www.naver.com");
+                    intent = new Intent(Intent.ACTION_VIEW, uri);
+                    break;
+                case R.id.btn_map:
+                    uri = Uri.parse("http://maps.google.com/maps?q=37.46642616688836,126.93282216760826");
+                    intent = new Intent(Intent.ACTION_VIEW, uri);
+                    break;
+                case R.id.btn_search:
+                    intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                    intent.putExtra(SearchManager.QUERY, "미림정보과학고");
+                    break;
+                case R.id.btn_sms:
+                    uri = Uri.parse("smsto:"+Uri.encode("010-7777-1235"));
+                    intent = new Intent(Intent.ACTION_SENDTO);
+                    intent.putExtra("sms_body", "안녕하세요? 여러분...");
+                    intent.setData(uri);
+                    break;
+                case R.id.btn_camera:
+                    intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    break;
+            }
+            startActivity(intent);
         }
     };
 }
